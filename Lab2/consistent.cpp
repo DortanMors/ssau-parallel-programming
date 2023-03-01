62ebb5bcc66dbb8e0933c559eb33af84184a54ac
Vyacheslav Fomin <2019-01376@students.ssau.ru>
2023-03-01 04:04:16 +0400

    consistent lab2

--- Lab2/consistent.cpp
+++ Lab2/consistent.cpp
@@ -1,13 +1,65 @@
 // consistent.cpp
-#include "lab2.h"
+
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include "common.h"
+#include "stdio.h"
+
+#define NMAX 6000000
+#define START_TIMES 12
+#define EXTENSION ".txt"
 
 using namespace std;
 
+double sum(double* array, long n)
+{
+  int i;
+  double sum = 0;
+  for (i = 0; i < n; ++i)
+	{
+    sum += array[i];
+  }
+  return sum;
+}
+
+double sum_q(double* array, long n)
+{
+  int i;
+  double reduce_sum = 0;
+  for (i = 0; i < NMAX; i++)
+	{
+    reduce_sum += sum_with_q(array[i]);
+  }
+  return reduce_sum;
+}
+
+void test_time(double (*method)(double*, long), std::ostream& out)
+{
+  double average_time = 0.0;
+  for (int t = 0; t < START_TIMES; ++t)
+	{
+    double* array = create_double_array(NMAX);
+    auto start = std::chrono::system_clock::now();
+    double sum = method(array, NMAX);
+    auto end = std::chrono::system_clock::now();
+    double result_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    average_time += result_time;
+    out << result_time << std::endl;
+    free(array);
+    printf("Total Sum = %10.2f \n", sum);
+    printf("TIME OF WORK IS %f microseconds\n", result_time);
+  }
+  average_time /= START_TIMES;
+  out << "Average: " << average_time << endl;
+}
+
 int main(int argc, char* argv[])
 {
-	fstream fout;
-	string path("results/");
-    fout.open(path + "consistent" + argv[1] + EXTENSION, ios::app);
+  fstream fout;
+  string path("results/");
+  fout.open(path + "consistent" + ((argc > 1) ? argv[1] : "")  + EXTENSION, ios::app);
 	
 	if (argc > 1 && string(argv[1]) == "q")
 	{
